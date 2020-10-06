48eaaa9f121a94e4053749510b44c4ff5b8776e5
Andrew Mason <amason@slack-corp.com>
2020-10-06 05:20:59 -0700

    Decode some layers

--- memcap/memcap.go
+++ memcap/memcap.go
@@ -10,7 +10,7 @@ import (
 	"github.com/google/gopacket/pcap"
 )
 
-var ValueRegexp = regexp.MustCompile(`VALUE (\S+) \S+ (\S+)`)
+var MemcCommandRegexp = regexp.MustCompile(`(\S+) (\S+) \S+ (\S+)`)
 
 type Memcap struct {
 	iface string
@@ -28,12 +28,12 @@ func NewMemcap(iface string, port int, d time.Duration) (*Memcap, error) {
 
 		d: d,
 
-		filter: fmt.Sprintf("port %d", port),
+		filter: fmt.Sprintf("tcp and port %d", port),
 	}, nil
 }
 
 func (mc *Memcap) Run() error {
-	handle, err := pcap.OpenLive(mc.iface, 1600, true, pcap.BlockForever)
+	handle, err := pcap.OpenLive(mc.iface, 3200, true, pcap.BlockForever)
 	if err != nil {
 		return err
 	}
@@ -42,21 +42,28 @@ func (mc *Memcap) Run() error {
 		return err
 	}
 
-	var (
-		// eth layers.Ethernet
-		// ip4 layers.IPv4
-		tcp layers.TCP
-	)
-
-	parser := gopacket.NewDecodingLayerParser(layers.LayerTypeTCP, &tcp)
-	decoded := []gopacket.LayerType{}
-
 	errCh := make(chan error)
 
+	packets := 0
+
 	go func() {
+		var (
+			eth layers.Ethernet
+			ip4 layers.IPv4
+			tcp layers.TCP
+			pl  gopacket.Payload
+		)
+
+		parser := gopacket.NewDecodingLayerParser(layers.LayerTypeEthernet, &eth, &ip4, &tcp, &pl)
+		decoded := []gopacket.LayerType{}
+
 		src := gopacket.NewPacketSource(handle, handle.LinkType())
 		for packet := range src.Packets() {
-			fmt.Printf("%s\n", packet.Dump())
+			//fmt.Printf("%s\n", packet.Dump())
+
+			// for _, layer := range packet.Layers() {
+			// 	fmt.Printf("- %s\n", layer.LayerType())
+			// }
 
 			if err := parser.DecodeLayers(packet.Data(), &decoded); err != nil {
 				errCh <- err
@@ -65,8 +72,12 @@ func (mc *Memcap) Run() error {
 
 			for _, layerType := range decoded {
 				switch layerType {
+				case layers.LayerTypeEthernet:
+				case layers.LayerTypeIPv4:
 				case layers.LayerTypeTCP:
-					fmt.Printf("%s\n", tcp.Payload)
+				case gopacket.LayerTypePayload:
+					packets++
+					fmt.Printf("%s\n", string(pl.Payload()))
 				}
 			}
 		}
@@ -76,7 +87,7 @@ func (mc *Memcap) Run() error {
 	case errCh <- err:
 		return err
 	case <-time.After(mc.d):
-		fmt.Printf("Finished after %s\n", mc.d.String())
+		fmt.Printf("Finished after %s. Processed %d packets\n", mc.d.String(), packets)
 	}
 
 	return nil
